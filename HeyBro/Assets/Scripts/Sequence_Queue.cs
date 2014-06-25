90918918c9738d81f823f2274c43391876a00b38
joshr2121 <joshr2121@gmail.com>
2014-06-25 16:18:21 -0400

    Added scene objects + music

---
+++ HeyBro/Assets/Scripts/Sequence_Queue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sequence_Queue : MonoBehaviour {
+
+	public GameObject[] sequenceSprites;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
