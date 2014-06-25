8cd412a67dcf4ce116faf7fd3672c84d074b5397
joshr2121 <joshr2121@gmail.com>
2014-06-25 15:49:21 -0400

    Added health bars

---
+++ HeyBro/Assets/Scripts/HealthBarEnemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarEnemy : MonoBehaviour {
+
+	public int max_XScale;
+	public int yScale;
+	public EnemyControls enemy;
+
+	// Use this for initialization
+	void Start () {
+		enemy = GameObject.Find ("Enemy").GetComponent<EnemyControls>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float percHealth = enemy.hp / 100;	//Should technically be / maxHP but it's okay 
+		transform.localScale = new Vector3 (max_XScale * percHealth, yScale, 1);
+		
+	}
+}
---
+++ HeyBro/Assets/Scripts/HealthBarPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarPlayer : MonoBehaviour {
+
+	public int max_XScale;
+	public int yScale;
+	public SequenceControls players;
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.Find ("Players").GetComponent<SequenceControls>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float percHealth = players.hp / 100;	//Should technically be / maxHP but it's okay 
+		transform.localScale = new Vector3 (max_XScale * percHealth, yScale, 1);
+		
+	}
+}
