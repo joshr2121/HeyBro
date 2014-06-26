35a42afd2f78786fb02f276908ce0db1a157c9ee
joshr2121 <joshr2121@gmail.com>
2014-06-26 17:07:48 -0400

    Fixed delay, maybe

--- HeyBro/Assets/Scripts/Sequence_Queue.cs
+++ HeyBro/Assets/Scripts/Sequence_Queue.cs
@@ -49,7 +49,7 @@ public class Sequence_Queue : MonoBehaviour {
 			sequenceObjects[i].renderer.enabled = false;
 		}
 		//Multiplying delay for easier debugging
-		seqDelay = 10*delay / yTicksBetweenPositions;
+		seqDelay = 5*delay / yTicksBetweenPositions;
 	}
 	public void MoveSpriteDown(){
 		float yTranslation = yDistanceBetweenPictograms / (float) yTicksBetweenPositions;
