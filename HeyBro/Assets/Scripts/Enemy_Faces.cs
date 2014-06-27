ab96a8fc9cd3805ac779a032da8ae075c197d89d
nkornek <nkornek@gmail.com>
2014-06-27 11:11:22 -0400

    Began head animations

---
+++ HeyBro/Assets/Scripts/Enemy_Faces.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Faces : MonoBehaviour {
+	public Sprite normal;
+	public Sprite angry;
+	public int state;
+
+	// Use this for initialization
+	void Start () {
+		state = 1;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		}
+
+	public void SetSprite (int sprite) {
+		switch (sprite) {
+
+		}
+	
+	}
+}
