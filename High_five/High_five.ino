63bda495c6c0493074a879d6824b2d22fc1f88d1
nkornek <nkornek@gmail.com>
2014-06-27 10:05:23 -0400

    Fixed arduino output

--- High_five/High_five.ino
+++ High_five/High_five.ino
@@ -32,10 +32,10 @@
 unsigned char PadNote[6] = {52,16,66,63,40,65};         // MIDI notes from 0 to 127 (Mid C = 60)
 
 // JOSH'S LAPTOP
-int PadCutOff[6] = {25,10,100,200,40,100};           // Minimum Analog value to cause a drum hit
+//int PadCutOff[6] = {25,10,100,200,40,100};           // Minimum Analog value to cause a drum hit
 
 // ACB'S LAPTOP
-//int PadCutOff[6] = {100,100,500,400,200,500};           // Minimum Analog value to cause a drum hit
+int PadCutOff[6] = {100,100,500,400,200,500};           // Minimum Analog value to cause a drum hit
 
 int MaxPlayTime[6] = {90,90,90,90,90,90};               // Cycles before a 2nd hit is allowed
 
@@ -80,8 +80,38 @@ void loop()
 
     if((hitavg > PadCutOff[pin]))
     {
-      Serial.println(pin+1);
-      Serial.flush();
+      if (analogRead(0) > analogRead(1) & analogRead(0) > analogRead(2) & analogRead(0) > PadCutOff[0])
+        {
+          Serial.println(1);
+          Serial.flush();
+        }
+      else if (analogRead(1) > analogRead(0) & analogRead(1) > analogRead(2) & analogRead(1) > PadCutOff[1])
+        {
+          Serial.println(2);
+          Serial.flush();
+        }
+      else if (analogRead(2) > analogRead(0) & analogRead(2) > analogRead(1) & analogRead(2) > PadCutOff[2])
+        {
+          Serial.println(3);
+          Serial.flush();
+        }
+      if (analogRead(3) > analogRead(4) & analogRead(3) > analogRead(5) & analogRead(3) > PadCutOff[3])
+        {
+          Serial.println(4);
+          Serial.flush();
+        }
+      else if (analogRead(4) > analogRead(3) & analogRead(4) > analogRead(5) & analogRead(4) > PadCutOff[4])
+        {
+          Serial.println(5);
+          Serial.flush();
+        }
+      else if (analogRead(5) > analogRead(3) & analogRead(5) > analogRead(4) & analogRead(5) > PadCutOff[5])
+        {
+          Serial.println(6);
+          Serial.flush();
+        }
+     // Serial.println(pin+1);
+     // Serial.flush();
     }
   } 
 }
